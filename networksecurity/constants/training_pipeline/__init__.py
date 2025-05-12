15c47ceb1f27f5ad3b33c201bd4e36a71a6b3fee
Zenith369 <prasham.work@gmail.com>
2025-05-12 16:09:45 +0530

    Data Ingestion component

---
+++ networksecurity/constants/training_pipeline/__init__.py
@@ -0,0 +1,28 @@
+import os
+import sys
+import pandas as pd
+import numpy as np
+
+
+'''
+Defining constants for the training pipeline
+'''
+
+TARGET_COLUMN : str = "Result"
+PIPELINE_NAME : str = "NetworkSecurity"
+ARTIFACT_DIR : str = "Artifacts"
+FILE_NAME : str = "phisingData.csv"
+TRAIN_FILE_NAME : str = "train.csv"
+TEST_FILE_NAME : str = "test.csv"
+
+
+'''
+Data Ingestion Constants start with DATA_INGESTION
+'''
+
+DATA_INGESTION_COLLECTION_NAME : str = "NetworkData"
+DATA_INGESTION_DATABASE_NAME : str = "NETWORKAI"
+DATA_INGESTION_DIR_NAME : str = "data_ingestion"
+DATA_INGESTION_FEATURE_STORE_DIR : str = "feature_store"
+DATA_INGESTION_INGESTED_DIR : str = "ingested"
+DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO : float = 0.2
