bfb857ab93142c449235e26ed9eeee69bc34ccd9
Zenith369 <prasham.work@gmail.com>
2025-09-22 16:28:26 +0530

    test script modified

--- test_mongodb.py
+++ test_mongodb.py
@@ -1,11 +1,12 @@
-
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
+from dotenv import load_dotenv
+load_dotenv()
 
-uri = ""
+MONGO_DB_URL = os.getenv("MONGO_DB_URL")
 
 # Create a new client and connect to the server
-client = MongoClient(uri, server_api=ServerApi('1'))
+client = MongoClient(MONGO_DB_URL, server_api=ServerApi('1'))
 
 # Send a ping to confirm a successful connection
 try:
