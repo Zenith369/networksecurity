ac1cc0519df9135d306f9168a060b960097ea3a6
Zenith369 <prasham.work@gmail.com>
2025-09-22 16:29:49 +0530

    test script modified

--- test_mongodb.py
+++ test_mongodb.py
@@ -1,6 +1,7 @@
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 from dotenv import load_dotenv
+import os
 load_dotenv()
 
 MONGO_DB_URL = os.getenv("MONGO_DB_URL")
